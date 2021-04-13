0a6c68bc734ce31f65b47b05c63c0f8c24d64caf
EtienneDouillard <edouillard@outlook.com>
2021-04-13 10:34:23 +0200

    laste version sans modeles

--- App_module/views.py
+++ App_module/views.py
@@ -4,6 +4,8 @@ from flask import Flask ,flash, render_template,jsonify,request, send_file,url_f
 
 from flask_sqlalchemy import SQLAlchemy 
 
+from sklearn.metrics import mean_squared_error
+from tensorflow.keras.utils import plot_model
 from tensorflow import keras
 import tensorflow as tf
 import pandas as pd
@@ -14,6 +16,8 @@ import random
 import json
 import io
 import csv
+import os
+
 
 """
 dTest = pd.read_csv('App_module//test10000.csv')
@@ -71,76 +75,49 @@ x2 = 50*np.sqrt(np.exp(-((k_vec)^2)/5)) * np.random.normal(size=50)
 
 
 #########################################################
-###################### Données  #########################
-#########################################################
-
-"""
-dTrain = pd.read_csv('App_module//DATA//trainFixe.csv')
-dTest = pd.read_csv('App_module//DATA//testFixe.csv')
-dValid = pd.read_csv('App_module//DATA//validFixe.csv')
-dBase= pd.read_csv('App_module//DATA//baseTestFixe.csv')
-
-dataBase =  pd.DataFrame(data=dBase)
-dataTrain = pd.DataFrame(data=dTrain) #création nouvelle dataframe
-dataTest = pd.DataFrame(data=dTest) #création nouvelle dataframe
-dataValid = pd.DataFrame(data=dValid) #création nouvelle dataframe
-
-
-##Données d'entrainement 
-x_train = dataTrain['x_train']
-y_train = dataTrain['y_train']
-x_train = np.array(x_train).reshape(int(len(x_train)/50),1,50)
-y_train = np.array(y_train).reshape(int(len(y_train)/50),1,50)
-
-##Données de validation 
-x_valid = dataValid['x_valid']
-y_valid = dataValid['y_valid']
-x_valid = np.array(x_valid).reshape(int(len(x_valid)/50),1,50)
-y_valid = np.array(y_valid).reshape(int(len(y_valid)/50),1,50)
-
-##Données de test 
-x_test = dataTest['x_test']
-y_test = dataTest['y_test']
-lengthTest = int(len(x_test)/50)
-x_test = np.array(x_test).reshape(lengthTest,1,50)
-y_test = np.array(y_test).reshape(lengthTest,1,50)
-
-##Données de base, données d'entrée  
-x_base = dataBase['x_base']
-x_part = dataBase['x_part']
-x_base = np.array(x_base).reshape(int(len(x_base)/50),50)
-x_part = np.array(x_part).reshape(int(len(x_part)/50),50)
-
-"""
-
-#########################################################
 ######################  Appli  ##########################
 #########################################################
 
 app = Flask(__name__)
 
-modelMSE = keras.models.load_model('MSE_modele\modelMSE')
+modelMSE = keras.models.load_model('modeles\Algo_modeles_Reconstruit_FinalV3_1\modelMSE')
+
+#modelMSE = keras.models.load_model('modeles\MSE_modele\modelMSE')
+
+
+modeleFixe0 = keras.models.load_model('modeles\Bests_algo_modelesFixe_indice0\modelMSE')
+modeleFixe10 = keras.models.load_model('modeles\Bests_algo_modelesFixe_indice10\modelMSE')
+modeleFixe18 = keras.models.load_model('modeles\Bests_algo_modelesFixe_indice18\modelMSE')
+modeleFixe27 = keras.models.load_model('modeles\Bests_algo_modelesFixe_indice27\modelMSE')
+modeleFixe36 = keras.models.load_model('modeles\Bests_algo_modelesFixe_indice36\modelMSE')
+modeleFixe45 = keras.models.load_model('modeles\Bests_algo_modelesFixe_indice45\modelMSE')
+
 
 fonction_reconstruite=[]
 #########################################################
 ######################  Def  ############################
 #########################################################    
 
-def prediction(globale_reconstruction,globale_cut):
+def prediction(modele,globale_reconstruction,globale_cut):
 
     #print("globale_reconstruction=",globale_reconstruction)
-    pred=modelMSE.predict(tf.keras.utils.normalize(globale_reconstruction))
+    pred=modele.predict(tf.keras.utils.normalize(globale_reconstruction))
 
     pred=pred.reshape(50)
     #print("pred=",pred)
     
+    MSE = mean_squared_error(globale_reconstruction,pred)
     #Nouvelle courbe prédite 
     predictionCourbe = np.full(50,np.nan)
     finale_prediction=[0]*len(pred)
 
     #remplacer les valeurs pour faire une seule et unique courbe à plot 
+ 
    
+    """
     for j in range(50):
+        
+
         if(j+1<49):
             if (np.isnan(globale_cut[j+1]) ):
                 predictionCourbe[j] = globale_cut[j]
@@ -150,27 +127,35 @@ def prediction(globale_reconstruction,globale_cut):
         if (np.isnan(globale_cut[j])):
             predictionCourbe[j] = pred[j]
         finale_prediction[j]=json.dumps(float(predictionCourbe[j]))
-
-  
-  
     """
+  
+    calcul_diff=0
+    
     if not np.isnan(globale_cut[0]):
         diff = pred[0] - globale_cut[0]
     else:
-        diff = pred[-1] - globale_cut[-1]
-
+        diff = 0
+        calcul_diff=1
 
     for j in range(50):
+    
         if(j+1<49):
-            if (np.isnan(globale_cut[j+1]) ):
+            if (np.isnan(globale_cut[j+1]) and calcul_diff==0 ):
+                
+                diff = pred[j]-globale_cut[j]
+                print("diff",diff)
+                calcul_diff=1
                 predictionCourbe[j] = globale_cut[j]
-        if(j-1>0):
+        if(j-1>0):       
             if (np.isnan(globale_cut[j-1])):
+                
+
                 predictionCourbe[j] = globale_cut[j]
         if (np.isnan(globale_cut[j])):
             predictionCourbe[j] = pred[j] - diff
-    """   
-
+     
+        finale_prediction[j]=json.dumps(float(predictionCourbe[j]))
+   
     return finale_prediction
 
 def cut_courbe(y,idx,partiePartielle):
@@ -232,6 +217,9 @@ def index():
     return render_template('accueil.html')
 
 
+@app.route('/NosGestClimat')
+def application(): 
+    return render_template('NosGestClimat.html')
 
 @app.route('/App_module/templates/accueil.html')
 def accueil(): 
@@ -285,13 +273,7 @@ def prediction_courbe():
 
     else:
         return render_template('courbes.html') 
-    
-    #possible_saisie = {    
-    #            1 : "Données d'entrée présélectionnées",
-    #            2 : "Données d'entrée sélectionnées depuis un fichier csv ",
-    #            3 : "Données d'entrée sélectionnées à la main ",
-    #            }
-
+  
 
     ######################################
     # Choix 1                            #
@@ -313,23 +295,33 @@ def prediction_courbe():
         if (bruit==0):
             choix_courbe = {    
                     1 : [(x/50)**2 for x in range(50)],
-                    2 : [np.exp(x/50) for x in range(50)],
-                    3 : [2*(x/50) for x in range(50)],
-                    4 : [-np.sqrt(x/50) for x in range(50)],
-                    5 : [np.sin(2*np.pi*x/50) for x in range(50)],
+                    2 : [(x/50)**5 - (x/50)**3 for x in range(50)],
+                    3 : [-np.exp(x/50) for x in range(50)],
+                    4 : [2*np.exp(x/50) for x in range(50)],
+                    5 : [2*(x/50) for x in range(50)],
+                    6 : [-np.sqrt(x/50) for x in range(50)],
+                    7 : [np.sqrt(x/50) for x in range(50)],
+                    8 : [np.sin(2*np.pi*x/50) for x in range(50)],
+                    9 : [np.cos(2*np.pi*x/50) for x in range(50)],
+                    10 : [-np.tan(x/50) for x in range(50)],
                     }
 
         else:
             choix_courbe = {    
                     1 : [(x/50)**2 for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
-                    2 : [np.exp(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
-                    3 : [2*(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
-                    4 : [np.sqrt(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
-                    5 : [np.sin(2*np.pi*x/50) for x in range(50)]+ np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    2 : [(x/50)**5 - (x/50)**3 for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    3 : [-np.exp(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    4 : [2*np.exp(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    5 : [2*(x/50) for x in range(50)]+ np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    6 : [-np.sqrt(x/50) for x in range(50)]+ np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    7 : [np.sqrt(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    8 : [np.sin(2*np.pi*x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    9 : [np.cos(2*np.pi*x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    10 : [-np.tan(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
                 }
         
         
-        if(indice_type_courbe1!=6):
+        if(indice_type_courbe1!=7):
            
             imputationSelect=request.form.getlist('imputationSelect')
 
@@ -345,23 +337,39 @@ def prediction_courbe():
                     5 : 36,
                     6 : 45,
                 }
+          
              #str pour légende type de courbe penser à modifer lors d'ajout de nouveaux modèles. 
             choix_type_courbe={
                     1 : "x**2",
-                    2 : "exp(x)",
-                    3 : "2x",
-                    4 : "-sqrt(x)",
-                    5 : "sin(x)",
+                    2 : "x**5 - x**3",
+                    3 : "-exp(x)",
+                    4 : "-exp(x)",
+                    5 : "2x",
+                    6 : "-sqrt(x)",
+                    7: "+sqrt(x",
+                    8 : "sin(x)",
+                    9 : "cos(x)",
+                    10 : "tan(x)", 
                 }
+             #Choix des modeles possibles en fonction de l'imputation 
+            choix_modele ={
+                    1 : modeleFixe0,
+                    2 : modeleFixe0,
+                    3 : modeleFixe0,
+                    4 : modeleFixe0,
+                    5 : modeleFixe0,                
+            }
 
         
-        
+            #choix du modèle en fonction de l'indice d'imputation 
+            print("indice_imputationSelect",indice_imputationSelect)
+            modele_preselection=choix_modele.get(indice_imputationSelect,-1)
             
             #Légende type de courbe 
             type_courbe=choix_type_courbe.get(indice_type_courbe1,-1)
 
             #légende indice d'imputation 
-            if (indice_imputationSelect != 6):
+            if (indice_imputationSelect != 11):
                 indiceStart = choix_imputation.get(indice_imputationSelect,-1)
                 indiceEnd = choix_imputation.get((indice_imputationSelect+1),-1)
                 indice_imputation = [indiceStart,indiceEnd]
@@ -375,15 +383,14 @@ def prediction_courbe():
             y = choix_courbe.get(indice_type_courbe1,-1)
             fonction_cut1,fonction_reconstruite1=cut_courbe(y,indiceStart,indiceEnd-indiceStart)
        
-
-
-
+       
 
         #Reconstruction et prédiction données manuelles  
         globale_reconstruction1=(np.array(fonction_reconstruite1)).reshape(1,1,50)
         globale_cut1=(np.array(fonction_cut1)).reshape(50)
 
-        finale_prediction1=prediction(globale_reconstruction1,globale_cut1)
+        #appel de la fonction predict avec le modele spécial en fonction de l'imputation. 
+        finale_prediction1=prediction(modele_preselection,globale_reconstruction1,globale_cut1)
 
         #Données fonctionnelles reconstruites suite aux données présélectionnées 
         fonction_cut1_2=[0]*(len(fonction_cut1))
@@ -394,7 +401,10 @@ def prediction_courbe():
             fonction_cut1_2[j]=(json.dumps(float(fonction_cut1[j])))
             fonction_reconstruite1_2[j]=(json.dumps(float(fonction_reconstruite1[j])))
 
-        return render_template('courbes.html',finale_prediction=finale_prediction1,fonction_cut=fonction_cut1_2,type_courbe=type_courbe,indice_imputation=indice_imputation,qte_bruit=bruit)
+        #choix type modèle 
+        indice_modele=json.dumps(indice_imputationSelect)
+
+        return render_template('courbes.html',finale_prediction=finale_prediction1,fonction_cut=fonction_cut1_2,type_courbe=type_courbe,indice_imputation=indice_imputation,qte_bruit=bruit,modele=indice_modele)
 
         
     ################
@@ -440,7 +450,7 @@ def prediction_courbe():
         globale_reconstruction=(np.array(fonction_reconstruite)).reshape(1,1,50)
         globale_cut=(np.array(fonction_cut)).reshape(50)
 
-        finale_prediction=prediction(globale_reconstruction,globale_cut)
+        finale_prediction=prediction(modelMSE,globale_reconstruction,globale_cut)
 
         #données fonctionnelles reconstruites suite auc données préselectionnées 
         fonction_cut2=[0]*(len(fonction_cut))
@@ -450,8 +460,10 @@ def prediction_courbe():
         for j in range(50):
             fonction_cut2[j]=(json.dumps(float(fonction_cut[j])))
             fonction_reconstruite2[j]=(json.dumps(float(fonction_reconstruite[j])))
-    
-        return render_template('courbes.html',finale_prediction=finale_prediction,fonction_cut=fonction_cut2,type_courbe=type_courbe,indice_imputation=indice_imputation,qte_bruit=bruit)
+
+
+        #modele=7 --> Le modele principal 
+        return render_template('courbes.html',finale_prediction=finale_prediction,fonction_cut=fonction_cut2,type_courbe=type_courbe,indice_imputation=indice_imputation,qte_bruit=bruit,modele=7)
 
 
       
@@ -474,41 +486,57 @@ def prediction_courbe():
         if (bruit==0):
             choix_courbe = {    
                     1 : [(x/50)**2 for x in range(50)],
-                    2 : [np.exp(x/50) for x in range(50)],
-                    3 : [2*(x/50) for x in range(50)],
-                    4 : [-np.sqrt(x/50) for x in range(50)],
-                    5 : [np.sin(2*np.pi*x/50) for x in range(50)],
+                    2 : [(x/50)**5 - (x/50)**3 for x in range(50)],
+                    3 : [-np.exp(x/50) for x in range(50)],
+                    4 : [2*np.exp(x/50) for x in range(50)],
+                    5 : [2*(x/50) for x in range(50)],
+                    6 : [-np.sqrt(x/50) for x in range(50)],
+                    7 : [np.sqrt(x/50) for x in range(50)],
+                    8 : [np.sin(2*np.pi*x/50) for x in range(50)],
+                    9 : [np.cos(2*np.pi*x/50) for x in range(50)],
+                    10 : [-np.tan(x/50) for x in range(50)],
                     }
+
         else:
             choix_courbe = {    
                     1 : [(x/50)**2 for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
-                    2 : [np.exp(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
-                    3 : [2*(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
-                    4 : [np.sqrt(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
-                    5 : [np.sin(2*np.pi*x/50) for x in range(50)]+ np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    2 : [(x/50)**5 - (x/50)**3 for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    3 : [-np.exp(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    4 : [2*np.exp(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    5 : [2*(x/50) for x in range(50)]+ np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    6 : [-np.sqrt(x/50) for x in range(50)]+ np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    7 : [np.sqrt(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    8 : [np.sin(2*np.pi*x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    9 : [np.cos(2*np.pi*x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
+                    10 : [-np.tan(x/50) for x in range(50)] + np.random.normal(size=50,loc=0,scale=np.sqrt(bruit/500)),
                 }
         
         #str pour légende type de courbe penser à modifer lors d'ajout de nouveaux modèles. 
         choix_type_courbe={
-                1 : "x**2",
-                2 : "exp(x)",
-                3 : "2x",
-                4 : "-sqrt(x)",
-                5 : "sin(x)",
-            }
+                    1 : "x**2",
+                    2 : "x**5 - x**3",
+                    3 : "-exp(x)",
+                    4 : "-exp(x)",
+                    5 : "2x",
+                    6 : "-sqrt(x)",
+                    7: "+sqrt(x",
+                    8 : "sin(x)",
+                    9 : "cos(x)",
+                    10 : "tan(x)", 
+                }
 
         #print("indice_type_courbe3",indice_type_courbe3)
 
-        if(indice_type_courbe3!=6):
+        if(indice_type_courbe3!=11):
             
           
-            indice3 = request.form.get("indice3")
-            taille3 = request.form.get("taille3")
+            indiceStart3 = request.form.get("indiceStart3")
+            indiceStop3 = request.form.get("indiceStop3")
             
             #condition erreur si les entrés ne sont pas des chiffres 
-            if  indice3.isdigit() and taille3.isdigit() :
-                indice3 = int(indice3)
-                taille3 = int(taille3)
+            if  indiceStart3.isdigit() and indiceStop3.isdigit() :
+                indiceStart3 = int(indiceStart3)
+                indiceStop3 = int(indiceStop3)
 
             else:
                 return render_template('courbes.html') 
@@ -516,17 +544,17 @@ def prediction_courbe():
            
             #cut courbe 
             y = choix_courbe.get(indice_type_courbe3,-1)
-            fonction_cut3,fonction_reconstruite3=cut_courbe(y,indice3,taille3)
+            fonction_cut3,fonction_reconstruite3=cut_courbe(y,indiceStart3,indiceStop3-indiceStart3)
             
             #Légende indice d'imputation et type de courbe 
-            indice_imputation = [indice3,indice3+taille3]
+            indice_imputation = [indiceStart3,indiceStop3]
             type_courbe=choix_type_courbe.get(indice_type_courbe3,-1)
 
         ##Reconstruction et prédiction données manuelles  
         globale_reconstruction3=(np.array(fonction_reconstruite3)).reshape(1,1,50)
         globale_cut3=(np.array(fonction_cut3)).reshape(50)
 
-        finale_prediction3=prediction(globale_reconstruction3,globale_cut3)
+        finale_prediction3=prediction(modelMSE,globale_reconstruction3,globale_cut3)
 
         #données fonctionnelles reconstruites suite auc données manuelles 
         fonction_cut3_2=[0]*(len(fonction_cut3))
@@ -537,8 +565,13 @@ def prediction_courbe():
             fonction_cut3_2[j]=(json.dumps(float(fonction_cut3[j])))
             fonction_reconstruite3_2[j]=(json.dumps(float(fonction_reconstruite3[j])))
 
+        #summary=modelMSE.summary()
+        #plot_model(modelMSE, to_file='model.png')
 
-        return render_template('courbes.html',finale_prediction=finale_prediction3,fonction_cut=fonction_cut3_2,type_courbe=type_courbe,indice_imputation=indice_imputation,modele=modelMSE,qte_bruit=bruit)
+        
+
+
+        return render_template('courbes.html',finale_prediction=finale_prediction3,fonction_cut=fonction_cut3_2,type_courbe=type_courbe,indice_imputation=indice_imputation,modele=7,qte_bruit=bruit)
 
         
 
